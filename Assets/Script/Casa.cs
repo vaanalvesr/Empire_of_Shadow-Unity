653eba1363429507c34a51119bf954244a6dbe1d
Vanderson Alves <vandersonalves15@gmail.com>
2017-07-04 22:17:49 -0300

    Adição das 3 primeiras classes de pecas

---
+++ Assets/Script/Casa.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casa : MonoBehaviour {
+
+    public Sprite casaImagem;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = casaImagem;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Peca.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peca : MonoBehaviour {
+
+    public Sprite red;
+    public Sprite blue;
+    private SpriteRenderer spriteRenderer;
+    public int life;
+    public int attack;
+    public int currentLife;
+    public int defense;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
